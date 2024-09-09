2e9bf85c070c9706e7b12aea5d1bea62507509d3
maria-aragao <mariafloraragao1@gmail.com>
2024-09-09 10:06:17 -0300

    Atividade de calcular os segundos

---
+++ Atividade 05.py
@@ -0,0 +1,6 @@
+# Escreva um programa que receba um valor em minutos e converta para segundos.
+
+minutos = int(input("O valor dos minutos"))
+segundos = minutos * 60
+
+print(f"{minutos} minutos tem {segundos} segundos")
